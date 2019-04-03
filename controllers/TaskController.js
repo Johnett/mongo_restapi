788cf4bfd4098c6a2a1e7c7520bfd2a8cd30ae08
Johnettdigitxp <johnett.mathew@digitxp.com>
2019-04-03 17:13:45 +0530

    last change reverted

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -47,9 +47,7 @@ exports.deleteTask = (req, res) => {
     if (err) {
       res.status(404).send(err);
     }
-    if(!item){
-      res.status(404).json({ message: "User not found"});
-    }
+
     res.status(200).json({ message: "Task successfully deleted" });
   });
 };
